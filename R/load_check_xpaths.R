e81d83d3b15172add44011a28795890e9b3039db
Matt Jones <gitcode@magisa.org>
2021-01-22 18:08:20 -0900

    Refactor load_check_xpaths to pass R CMD check.
    In R packages, "scripts" are not allowed, and so anythin in the R directory needs to be functions. I refactored to create functions, and properly escaped the tidyverse pipe operations to eliminate warnings about global variables. R CMD check is passing again.

--- R/load_check_xpaths.R
+++ R/load_check_xpaths.R
@@ -1,65 +1,88 @@
-library(xml2)
-library(dplyr)
-library(purrr)
-library(tidyr)
+utils::globalVariables(c("."))
 
-# this script grabs a table of all xpaths, the check they correspond to, and
+# Summarize checks
+#
+# This function grabs a table of all xpaths, the check they correspond to, and
 # their dialect for ease of review. It downloads the metadig-checks repo, gets
 # summary information on the FAIR suite, and then parses the individual xml
 # files for each check in the suite
-
-
-# download the metadig-checks repo to a tempdir and unzip it
+#
+# Download the metadig-checks repo to a tempdir and unzip it
 # currently, this gets the tip of the master branch. as xpaths for checks are
 # reviewed and/or changed, this should will change to the tip of the branch
 # where those changes are made. this file is deliberarely not cached since it
 # is expected to change
-t <- tempdir()
-download.file(url = "https://github.com/NCEAS/metadig-checks/archive/master.zip",
-              destfile = paste0(t, "/metadig.zip"))
+#' @import dplyr
+#' @import xml2
+#' @import purrr
+#' @import tidyr
+#' @importFrom rlang .data
+#' @importFrom utils download.file
+#' @importFrom utils unzip
+summarize_checks <- function() {
+    t <- tempdir()
+    download.file(url = "https://github.com/NCEAS/metadig-checks/archive/master.zip",
+                  destfile = paste0(t, "/metadig.zip"))
 
-z <- unzip(paste0(t, "/metadig.zip"), exdir = t)
+    z <- unzip(paste0(t, "/metadig.zip"), exdir = t)
 
+    # read in xml file listing the check names
+    fair_suite_def <-
+        read_xml(paste0(t, "/metadig-checks-master/src/suites/FAIR-suite.xml"))
 
-# read in xml file listing the check names
-fair_suite_def <- read_xml(paste0(t, "/metadig-checks-master/src/suites/FAIR-suite.xml"))
+    # extract check names and correct some inconsistent names
+    check_names <- fair_suite_def %>%
+        xml_find_all("./check/id") %>%
+        xml_text() %>%
+        gsub(".1|.2", "", .) %>%
+        gsub("resource.abstractLength.sufficient",
+             "resource.abstractLength",
+             .)
 
-# extract check names and correct some inconsistent names
-check_names <- fair_suite_def %>%
-    xml_find_all("./check/id") %>%
-    xml_text() %>%
-    gsub(".1|.2", "", .) %>%
-    gsub("resource.abstractLength.sufficient", "resource.abstractLength", .)
+    # extract check types
+    check_type <- fair_suite_def %>%
+        xml_find_all("./check/type") %>%
+        xml_text()
 
-# extract check types
-check_type <- fair_suite_def %>%
-    xml_find_all("./check/type") %>%
-    xml_text()
+    # extract check levels (required or optional)
+    check_level <- fair_suite_def %>%
+        xml_find_all("./check/level") %>%
+        xml_text()
 
-# extract check levels (required or optional)
-check_level <- fair_suite_def %>%
-    xml_find_all("./check/level") %>%
-    xml_text()
+    # create data frame of checks
+    fair_checks <-
+        data.frame(check_names, check_type, check_level, stringsAsFactors = F)
 
-# create data frame of checks
-fair_checks <- data.frame(check_names, check_type, check_level, stringsAsFactors = F)
+    # run get description function over all checks
+    fair_checks$description <- map_chr(check_names, get_description, t)
+
+    # extract xpaths for all checks
+    xpaths <- purrr::map(check_names, parse_xpath, t)
+
+    fair_checks$xpath <- xpaths
+    # expand data frame so each row corresponds to an xpath
+    fair_checks_ex <- fair_checks %>%
+        unnest(cols = .data$xpath)
+
+    # run function to assign dialect to each xpath
+    fair_checks_ex$dialect <-
+        map_chr(fair_checks_ex$xpath, assign_dialect)
+
+    return(fair_checks_ex)
+}
 
 # define function to read individual check file and get
 # check description
-get_description <- function(check_name){
+get_description <- function(check_name, t) {
     base_path <- paste0(t, "/metadig-checks-master/src/checks/")
     t <- read_xml(paste0(base_path, check_name, ".xml")) %>%
         xml_find_all("./description") %>%
         xml_text()
-
 }
-
-# run get description function over all checks
-fair_checks$description <- map_chr(check_names, get_description)
 
 # define function to read check file in, then extract and clean
 # all xpaths contained within the check
-parse_xpath <- function(check_name){
+parse_xpath <- function(check_name, t) {
     base_path <- paste0(t, "/metadig-checks-master/src/checks/")
     xps <- read_xml(paste0(base_path, check_name, ".xml")) %>%
         xml_find_all("./selector/xpath") %>%
@@ -76,28 +99,19 @@ parse_xpath <- function(check_name){
         grep("[a-z].*", ., value = T)
 }
 
-# extract xpaths for all checks
-xpaths <- purrr::map(check_names, parse_xpath)
-
-fair_checks$xpath <- xpaths
-# expand data frame so each row corresponds to an xpath
-fair_checks_ex <- fair_checks %>%
-    unnest(cols = xpath)
-
 # define function to assign metadata to dialect based
 # on the start of the xpath
-assign_dialect <- function(xpath){
-    if (startsWith(xpath, "/*/")){
+assign_dialect <- function(xpath) {
+    if (startsWith(xpath, "/*/")) {
         return("ISO")
-    } else if (startsWith(xpath, "/eml")){
+    } else if (startsWith(xpath, "/eml")) {
         return("EML")
-    } else if (startsWith(xpath, "/resource")){
+    } else if (startsWith(xpath, "/resource")) {
         return("DataCite")
-    } else if (startsWith(xpath, "/DryadDataFile")){
+    } else if (startsWith(xpath, "/DryadDataFile")) {
         return("Dryad")
-    } else return(NA)
-
+    } else {
+        return(NA)
+    }
 }
-# run function to assign dialect to each xpath
-fair_checks_ex$dialect <- map_chr(fair_checks_ex$xpath, assign_dialect)
 
